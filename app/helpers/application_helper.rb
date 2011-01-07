4a5edca7d019adc61fef2e63936fab5434bd1fad
Andris Stikans <andris2005@inbox.lv>
2011-01-08 00:30:46 +0200

    Fix navigation - refactor -> links to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def menu_button(text)
+    text + "<span class='tab-l'></span><span class='tab-r'></span>"
+  end
+  
+  def new_button(controller)
+    if controller == "subjects" ||
+        controller == "books" ||
+        controller == "music_artists" ||
+        controller == "movies"
+    
+      link_to "New #{controller.underscore.humanize.singularize.downcase}", new_polymorphic_path(controller.singularize)
+    end
+  end
+  
 end
