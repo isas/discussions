1280f8d42e5205eca8c5664becfb04c7d56d5308
Ingus Skaistkalns <ingus.skaistkalns@gmail.com>
2011-01-07 20:28:38 +0200

    Added bundler :D

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source "http://rubygems.org"
+
+gem "rails", "2.3.10"
+gem "mysql", "2.8.1"
+  
+group :test do
+  gem "rspec", "1.3.1"
+  gem "rspec-rails", "1.3.3"
+  gem "rcov", "0.9.9"
+  gem "metric_fu", "2.0.1"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -20,12 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "mysql", :version => "2.8.1"
-  config.gem "rspec", :lib => false, :version => "1.3.1"
-  config.gem "rspec-rails", :lib => false, :version => "1.3.3"
-  config.gem "rcov", :lib => false, :version => "0.9.9"
-  config.gem "metric_fu", :lib => false, :version => "2.0.1"
-
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
