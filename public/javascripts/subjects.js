763c8ed11c930ddf2297dc6b9e30a5ab287a4f23
Andris Stikans <andris2005@inbox.lv>
2011-01-08 12:59:28 +0200

    Select subject and go to category new page

--- public/javascripts/subjects.js
+++ public/javascripts/subjects.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     type_title.call(this);
     new_subject.call(this);
+    new_subject_change.call(this);
 })
 
 function type_title(){
@@ -14,17 +15,39 @@ function type_title(){
 function new_subject()
 {
     $(".new_subject").live("click", function(){
-    
-        var html = "<div title='Subject type'><div class='new' style='display:none;' > TODO <br/> Choose subject type and get right new page</div></div>";
+        var option0 = "<option value=''></option>";
+        var option1 = "<option value='music_artists'>Music artist</option>";
+        var option2 = "<option value='movies'>Movie</option>";
+        var option3 = "<option value='books'>Book</option>";
+        
+        var select_box = "<select id='subject_id' name='subject'>" +option0+option1+option2+option3 +"</select>";
+        var html = "<div title='Subject type'><div class='new' style='display:none;'> Chose category:"+select_box+"</div></div>";
         $(html).dialog({
+            dialogClass: 'alert',
             modal: true, 
-            width: 600, 
-            heigth: 600, 
+            minWidth: 300, 
+            minHeigth: 100,
+            maxWidth: 300, 
+            maxHeigth: 100,
             closeOnEscape: true, 
-            position: 'top'
+            position: 'center',
+            draggable: false,
+            resizable: false,
+            //hide: 'slide', 
+            buttons: { "Close": function() { $(this).dialog("close"); } }
         });
         $("div.new").fadeIn("slow");
     
         return false;
+    });
+}
+
+function new_subject_change()
+{
+    $("select#subject_id").live("change", function(){
+       var $this = $(this);
+       window.location = "/" + $this.val() + "/new"
+       $(".new").html("<div class='loading'></div>");
+       return false;
     });
 }
