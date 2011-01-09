8e7ebef9b8fe0ab63ee9a35f5eb2393b137a9b77
Andris Stikans <andris2005@inbox.lv>
2011-01-09 16:41:28 +0200

    Comments size fix

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -9,7 +9,7 @@ function save_comment()
         var params = $this.closest("form").serialize();
         var parent_div = $this.closest("div")
         
-        parent_div.html("<div class='loading'></div>");
+        parent_div.html("<div class='loading' style='display:block; height:200px;'></div>");
 
         $.ajax({
             url: "/comments/",
@@ -18,9 +18,18 @@ function save_comment()
             success: function(data) {
                 parent_div.html(data);
                 $(".need_to_show").fadeIn("slow");
+                
+                
+                if (data.search(/can't be blank/) == -1)
+                {
+                    var comments_size = parseInt($(".comment_size").val()) + 1
+                    $(".comment_size").val(comments_size)
+                    $(".comment_size_text").html("Comments (" + comments_size + ")");
+                }
             }
         });
         
         return false;
     });
 }
+
